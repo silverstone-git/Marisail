58a18e825da763c9072ffbc3f9ca1883779959b1
= <=>
2024-08-13 12:33:22 -0300

    sponsors responsive grid done

--- src/Components/Home/SponsorsRanking.jsx
+++ src/Components/Home/SponsorsRanking.jsx
@@ -16,19 +16,23 @@ const SponsorsRanking = ({ sponsors }) => {
   }, []); */
 
   return (
-    <Container
-      className='py-4'
-      style={{
-        display: 'grid',
-        gridTemplateColumns: ' repeat( auto-fill, minmax(180px, 1fr) )',
-      }}
-    >
-      {sponsors.ok &&
-        sponsors.result.map((el, i) => (
-          <Card key={i} className='text-center py-2'>
-            {el.company_name}
-          </Card>
-        ))}
+    <Container className='my-3'>
+      <h3>Our Sponsors</h3>
+
+      <Container
+        className='py-4'
+        style={{
+          display: 'grid',
+          gridTemplateColumns: ' repeat( auto-fill, minmax(180px, 1fr) )',
+        }}
+      >
+        {sponsors.ok &&
+          sponsors.result.map((el, i) => (
+            <Card key={i} className='text-center py-2 border-0'>
+              {el.company_name}
+            </Card>
+          ))}
+      </Container>
     </Container>
   );
 };
