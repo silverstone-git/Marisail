7ee965b5b033e03aec40e2c4aec1586947cd7c49
pragathyT <pragathyt95@gmail.com>
2024-08-20 09:06:17 +0530

    fixes for axios import error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,23 @@
 // import AppCarousel from '../components/AppCarousel';
-import { useEffect } from 'react';
-import SponsorsRanking from '../components/Home/SponsorsRanking';
-import axios from 'axios';
-import { useState } from 'react';
+// import { useEffect } from 'react';
+// import SponsorsRanking from '../components/Home/SponsorsRanking';
+// import { useState } from 'react';
 
 const Home = () => {
-  const [sponsors, setSponsors] = useState({ ok: false, result: [] });
-  useEffect(() => {
-    axios
-      .get('http://localhost:3001/sponsors')
-      .then((response) => {
-        console.log('001 Response from API--', response);
-        setMessage(response);
-        return response.data;
-      })
-      /* .then((data) => setSponsors(data)) */
-      .catch((error) => {
-        console.error('There was an error fetching the message!', error);
-      });
-  }, []);
+  // const [sponsors, setSponsors] = useState({ ok: false, result: [] });
+  // useEffect(() => {
+  //   axios
+  //     .get('http://localhost:3001/sponsors')
+  //     .then((response) => {
+  //       console.log('001 Response from API--', response);
+  //       setMessage(response);
+  //       return response.data;
+  //     })
+  //     /* .then((data) => setSponsors(data)) */
+  //     .catch((error) => {
+  //       console.error('There was an error fetching the message!', error);
+  //     });
+  // }, []);
   return (
     <>
       <main
