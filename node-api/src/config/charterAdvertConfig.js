bf447d37a022f2b39d9f7b146766c1a6f20fbb41
pragathyT <pragathyt95@gmail.com>
2024-09-15 08:50:40 +0530

    charter advert changes

--- node-api/src/config/charterAdvertConfig.js
+++ node-api/src/config/charterAdvertConfig.js
@@ -1,5 +1,306 @@
 export const CHARTER_ADVERT = [
-    
+  {
+    key: "marisailVesselId",
+    columnName: "Marisail Vessel ID",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "marisailCharterId",
+    columnName: "Marisail Charter ID",
+    tableName: "Accommodation_Location",
+  },
+
+  {
+    key: "guestCapacity",
+    columnName: "Guest_Capacity",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "bedroomConfiguration",
+    columnName: "Bedroom_Configuration",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "bathroomConfiguration",
+    columnName: "Bathroom_Configuration",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "crewAccommodations",
+    columnName: "Crew_Accommodation",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "accessibilityInformation",
+    columnName: "Accessibility_Information",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "cleaningAndMaintenanceProcedures",
+    columnName: "Maintenance_Procedures",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "vesselDecorAndSetupRequests",
+    columnName: "Yacht_Decor",
+    tableName: "Accommodation_Location",
+  },
+
+  {
+    key: "boardingPortArrivalTime",
+    columnName: "Boarding_Arrival_Time",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "boardingPortDepartureTime",
+    columnName: "Boarding_Departure_Time",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "summerCruisingAreas",
+    columnName: "Summer_Cruising_Areas",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "boardingPort",
+    columnName: "Boarding_Port",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "winterCruisingAreas",
+    columnName: "Winter_Cruising_Areas",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "disembarkationPort",
+    columnName: "Disembarkation_Port",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "embarkationAndDisembarkationLogistics",
+    columnName: "Logistics",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "disembarkationPortArrivalTime",
+    columnName: "Disembarkation_Arrival_Time",
+    tableName: "Accommodation_Location",
+  },
+  {
+    key: "dockingAndMooringInstructions",
+    columnName: "Mooring_Instructions",
+    tableName: "Accommodation_Location",
+  },
+
+  {
+    key: "skipperIncluded",
+    columnName: "Captain_Included",
+    tableName: "Requirements",
+  },
+  {
+    key: "crewIncluded",
+    columnName: "Crew_Included",
+    tableName: "Requirements",
+  },
+  {
+    key: "crewUniformPreferences",
+    columnName: "Crew_Uniform",
+    tableName: "Requirements",
+  },
+  {
+    key: "localCuisinePreferences",
+    columnName: "Cuisine_Preferences",
+    tableName: "Requirements",
+  },
+  {
+    key: "cateringRequired",
+    columnName: "Catering_Required",
+    tableName: "Requirements",
+  },
+  {
+    key: "carParkingAvailable",
+    columnName: "Car_Parking",
+    tableName: "Requirements",
+  },
+  {
+    key: "specialRequirementsRequests",
+    columnName: "Special_Requirements",
+    tableName: "Requirements",
+  },
+
+  { key: "smokingPolicy", columnName: "Smoking_Policy", tableName: "Policy" },
+  { key: "petFriendlyPolicy", columnName: "Pet_Policy", tableName: "Policy" },
+  {
+    key: "localRegulationsAndRestrictions",
+    columnName: "Local_Regulations",
+    tableName: "Policy",
+  },
+  {
+    key: "charterAgreementTermsAndConditions",
+    columnName: "Charter_TCs",
+    tableName: "Policy",
+  },
+  {
+    key: "environmentalPolicies",
+    columnName: "Environmental_Policies",
+    tableName: "Policy",
+  },
+  {
+    key: "waterConservationMeasures",
+    columnName: "Water_Conservation",
+    tableName: "Policy",
+  },
+  {
+    key: "wasteManagementProtocols",
+    columnName: "Waste_Management",
+    tableName: "Policy",
+  },
+  { key: "alcoholPolicy", columnName: "Alcohol", tableName: "Policy" },
+  {
+    key: "photographyAndVideographyPolicies",
+    columnName: "Photography_Policies",
+    tableName: "Policy",
+  },
+
+  {
+    key: "weatherContingencyPlans",
+    columnName: "Weather_Contingency",
+    tableName: "Safety_Measures",
+  },
+  {
+    key: "emergencyProcedures",
+    columnName: "Emergency_Procedures",
+    tableName: "Safety_Measures",
+  },
+  {
+    key: "medicalFacilitiesOnboard",
+    columnName: "Medical_Facilities",
+    tableName: "Safety_Measures",
+  },
+  {
+    key: "emergencyContacts",
+    columnName: "Emergency_Contacts",
+    tableName: "Safety_Measures",
+  },
+  {
+    key: "weatherForecastServices",
+    columnName: "Weather_Forecast",
+    tableName: "Safety_Measures",
+  },
+  {
+    key: "securityMeasures",
+    columnName: "Security_Measures",
+    tableName: "Safety_Measures",
+  },
+  {
+    key: "guestOrientationAndSafetyBriefing",
+    columnName: "Safety_Briefing",
+    tableName: "Safety_Measures",
+  },
+  {
+    key: "insuranceForGuestsPersonalBelongings",
+    columnName: "Guest_Insurance",
+    tableName: "Safety_Measures",
+  },
+  {
+    key: "insuranceCoverageDetails",
+    columnName: "Insurance_Coverage",
+    tableName: "Safety_Measures",
+  },
+
+  {
+    key: "summerRatePerNight",
+    columnName: "Summer_Rate_PN",
+    tableName: "Charter_Costs",
+  },
+  {
+    key: "winterRatePerWeek",
+    columnName: "Winter_Rate_PW",
+    tableName: "Charter_Costs",
+  },
+  {
+    key: "winterRatePerNight",
+    columnName: "Winter_Rate_PN",
+    tableName: "Charter_Costs",
+  },
+  {
+    key: "summerRatePerWeek",
+    columnName: "Summer_Rate_PW",
+    tableName: "Charter_Costs",
+  },
+  {
+    key: "securityDepositAmount",
+    columnName: "Deposit_Amount",
+    tableName: "Charter_Costs",
+  },
+  { key: "totalPrice", columnName: "Total_Price", tableName: "Charter_Costs" },
+  {
+    key: "refundableDeposit",
+    columnName: "Refundable_Deposit",
+    tableName: "Charter_Costs",
+  },
+  {
+    key: "additionalFuelCosts",
+    columnName: "Additional_Fuel",
+    tableName: "Charter_Costs",
+  },
+  {
+    key: "additionalFees",
+    columnName: "Additional_Fees",
+    tableName: "Charter_Costs",
+  },
+  {
+    key: "fuelIncluded",
+    columnName: "Fuel_Included",
+    tableName: "Charter_Costs",
+  },
+  {
+    key: "lateCheckInCheckOutFees",
+    columnName: "Late_Fees",
+    tableName: "Charter_Costs",
+  },
+  { key: "vat", columnName: "VAT", tableName: "Charter_Costs" },
+
+  {
+    key: "minimumNightsPolicy",
+    columnName: "Minimum_Nights",
+    tableName: "Dates",
+  },
+  { key: "datesAvailable", columnName: "Dates_Available", tableName: "Dates" },
+  {
+    key: "cancellationPolicy",
+    columnName: "Cancellation_Policy",
+    tableName: "Dates",
+  },
+
+  { key: "startDate", columnName: "Start_Date", tableName: "Dates" },
+  { key: "endDate", columnName: "End_Date", tableName: "Dates" },
+  { key: "numberNights", columnName: "Number_Nights", tableName: "Dates" },
+
+  {
+    key: "paymentTerms",
+    columnName: "Payment_Terms",
+    tableName: "Charter_Payment",
+  },
+  { key: "currency", columnName: "Currency", tableName: "Charter_Payment" },
+  {
+    key: "preferredPaymentMethods",
+    columnName: "Preferred_Payment",
+    tableName: "Charter_Payment",
+  },
+  {
+    key: "invoiceAndReceiptProcedures",
+    columnName: "Invoice_Receipt",
+    tableName: "Charter_Payment",
+  },
+
+  {
+    key: "calculatePriceAndPay",
+    columnName: "Calculate",
+    tableName: "Charter_Sales",
+  },
+  { key: "priceLabel", columnName: "Price_Label", tableName: "Charter_Sales" },
+  { key: "priceDrop", columnName: "Price_Drop", tableName: "Charter_Sales" },
 ];
 
 export const UNIQUE_TABLE = [
