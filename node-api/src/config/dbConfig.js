cc23882fe5c0a4ae49f671b29c8d0efb3a919c34
Alexander <aragonantar@gmail.com>
2025-01-06 21:59:43 -0500

    Add files via upload
    doing developer work

---
+++ node-api/src/config/dbConfig.js
@@ -0,0 +1,19 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const ENVIRONMENT = process?.env;
+
+const connection = mysql.createPool({
+  host: ENVIRONMENT.DB_HOST,
+  user: ENVIRONMENT.DB_USER,
+  password: ENVIRONMENT.DB_PASS,
+  database: ENVIRONMENT.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+
+export default connection;
